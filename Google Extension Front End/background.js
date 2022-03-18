d68281e88fa3a1946a926aa6fdd3e0d6f74ad6ff
Clay <claygifford1@gmail.com>
2022-03-18 15:37:49 -0500

    added app to firebase

---
+++ Google Extension Front End/background.js
@@ -0,0 +1,9 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  //...
+};
+
+const app = initializeApp(firebaseConfig);
+
