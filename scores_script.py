64a198315e1ade7ed44cdd28f8aabf30374643aa
Clay Gifford <claygifford1@gmail.com>
2022-03-28 02:53:23 -0500

    updated data collection split prof name and course

--- scores_script.py
+++ scores_script.py
@@ -6,6 +6,97 @@ import re
 import tabula as tb
 import pandas as pd
 
+def get_prof_names(df, verbose):
+
+    name_pattern = re.compile("[,. ]+")
+    #name_pattern = re.compile("[a-zA-z]*[,. ]+")
+    course_pattern = re.compile("[A-Z]{2,6}([-][A-Z0-9]+)+", re.IGNORECASE) # Need to find out why last ones aren't matching
+
+    size = df.shape[0]
+
+    lnames = [None] * size
+    fnames = [None] * size
+    course_names = [None] * size
+    course_numbers = [None] * size
+
+    for index, value in df["Instructor and Course"].iteritems():
+
+        value = value.strip()
+        fixed = value.split("\r")
+        value = ''.join(fixed)
+
+        split_by_name = name_pattern.split(value, maxsplit=2)
+
+        """
+        if verbose:
+            print(index)
+            print(split_by_name)
+            print()
+        """
+
+        lnames[index] = split_by_name[0]
+        fnames[index] = split_by_name[1]
+        leftover = split_by_name[2].split(":")
+
+        leftover[0] = leftover[0].strip()
+        leftover[1] = leftover[1].strip()
+
+        if verbose:
+            print(leftover)
+
+        match1 = course_pattern.search(leftover[0])
+        match2 = course_pattern.search(leftover[1])
+
+        if match1 != None:
+            course_numbers[index] = match1.group(0)
+            course_names[index] = leftover[1]
+        
+        if match2 != None:
+            course_numbers[index] = match2.group(0)
+            course_names[index] = leftover[0]
+
+
+    df["Lname"] = lnames
+    df["Fname"] = fnames
+    df["Course_Number"] = course_numbers
+    df["Course_Name"] = course_names
+
+    
+def clean_data(filepath, verbose):
+
+    columns_list = ["Instructor and Course", "Enrolled", "Responses", "Response Rate", 
+    "Prepardeness", "Communication", "Clarity", "Encouragement", "Availability"]
+
+    df = pd.read_csv(filepath, header=0)
+
+    if len(df.columns) < len(columns_list):
+
+        if verbose:
+            print("\nERROR: file {} not clean - incompatible size requirement\n".format(filepath))
+        return
+
+    df.dropna(axis=1, how="all", inplace=True)
+    df.columns = columns_list
+    df.drop("Response Rate", axis=1, inplace=True)
+
+    type_corrections_list = ["Enrolled", "Responses","Prepardeness", 
+    "Communication", "Clarity", "Encouragement", "Availability"]
+
+    for col in type_corrections_list:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+
+    df.dropna(inplace=True)
+
+    df.reset_index(drop=True, inplace=True)
+
+    if verbose:
+        print("data cleaned - dataframe head below")
+
+    get_prof_names(df, verbose)
+
+    print(df.head())
+    print("\n")
+
 def scrape_survey_data(verbose=False):
 
     page = requests.get("https://www.uta.edu/ier/student-feedback-survey/students.php")
@@ -16,9 +107,13 @@ def scrape_survey_data(verbose=False):
 
     soup = bs(page.content, 'html.parser')
 
+    """
+
     if verbose:
         print("\ncontents downloaded... Outputting content below\n\n")
         print(soup.prettify())
+
+    """
 
     UTA_site_path = "https://www.uta.edu/ier/student-feedback-survey/"
 
@@ -51,26 +146,9 @@ def scrape_survey_data(verbose=False):
             if verbose:
                 print("file converted to csv format")
 
-def clean_data(filepath):
+            if verbose:
+                print("cleaning data...")
 
-    columns_list = ["Instructor and Course", "Enrolled", "Responses", "Response Rate", 
-    "Prepardeness", "Communication", "Clarity", "Encouragement", "Availability"]
-
-    df = pd.read_csv(filepath, header=0)
-
-    df.dropna(axis=1, how="all", inplace=True)
-    df.columns = columns_list
-    df.drop("Response Rate", axis=1, inplace=True)
-
-    type_corrections_list = ["Enrolled", "Responses","Prepardeness", 
-    "Communication", "Clarity", "Encouragement", "Availability"]
-
-    for col in type_corrections_list:
-        df[col] = pd.to_numeric(df[col], errors='coerce')
-
-    df.dropna(inplace=True)
-
-    print(df.head())
+            clean_data(csv_path, verbose)
 
 scrape_survey_data(True)
-clean_data("Fall 2021.csv")
