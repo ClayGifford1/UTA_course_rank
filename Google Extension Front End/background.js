67c9278f65f431f3bc564d12064124d3eab2e640
Clay Gifford <claygifford1@gmail.com>
2022-04-23 15:37:36 -0500

    added database connection

--- Google Extension Front End/background.js
+++ Google Extension Front End/background.js
@@ -1,9 +1,18 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from '@firebase/app';
+import { getAnalytics } from '@firebase/analytics';
+import { getDatabase } from '@firebase/database'; 
 
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  //...
+  apiKey: "AIzaSyDbGq-_w7iBzjmCncRVHeUovDsZNuaID3Q",
+  authDomain: "course-rank.firebaseapp.com",
+  databaseURL: "https://course-rank-default-rtdb.firebaseio.com",
+  projectId: "course-rank",
+  storageBucket: "course-rank.appspot.com",
+  messagingSenderId: "303355690951",
+  appId: "1:303355690951:web:97eb35208734ced587a3e2",
+  measurementId: "G-9D8S1QQCY5"
 };
 
 const app = initializeApp(firebaseConfig);
-
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
